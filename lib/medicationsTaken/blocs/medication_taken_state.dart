24bd097eedcd5078af106a3b191f3efeff7d3a18
Rafooo6 <rafaelbravo032@gmail.com>
2025-06-19 02:57:25 -0500

    feat(reminders): add medication taken state management.

---
+++ lib/medicationsTaken/blocs/medication_taken_state.dart
@@ -0,0 +1,15 @@
+import 'package:tukuntech/home/domain/entities/PendingMedications.dart';
+import 'package:tukuntech/medicationsTaken/domain/entities/MedicationTaken.dart';
+
+abstract class MedicationTakenState {
+  const MedicationTakenState();
+
+}
+
+class InitialMedicationTakenState extends MedicationTakenState {}
+
+class LoadedMedicationTakenState extends MedicationTakenState {
+  final List<MedicationTaken> medicationTaken;
+
+  const LoadedMedicationTakenState({required this.medicationTaken});
+}
