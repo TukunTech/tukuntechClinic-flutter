f72ed96abeea0af3b5a5c1fa6cf9bd41382cc07a
Rafooo6 <rafaelbravo032@gmail.com>
2025-06-19 02:57:57 -0500

    feat(medicationsTaken): add medication taken event.

---
+++ lib/medicationsTaken/blocs/medication_taken_event.dart
@@ -0,0 +1,15 @@
+import 'package:tukuntech/medicationsTaken/domain/entities/MedicationTaken.dart';
+
+abstract class MedicationTakenEvent {
+  const MedicationTakenEvent();
+}
+class AddMedicationTakenEvent extends MedicationTakenEvent {
+  final MedicationTaken medicationTaken;
+  const AddMedicationTakenEvent({required this.medicationTaken});
+}
+
+class RemoveMedicationTakenEvent extends MedicationTakenEvent {
+  final int id;
+  const RemoveMedicationTakenEvent({required this.id});
+}
+class GetAllMedicationTakenEvent extends MedicationTakenEvent {}
