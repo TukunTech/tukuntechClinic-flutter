67579b8384aa7359071704e59ea21fcbc833825a
Erick <erickmaraza20@gmail.com>
2025-06-19 15:31:27 -0500

    feat(profile): implement ElderBloc for state management

---
+++ lib/features/profile/presentation/blocs/elder_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'elder_event.dart';
+import 'elder_state.dart';
+import '../../data/models/elder_dto.dart';
+import '../../data/datasources/elder_service.dart';
+
+class ElderBloc extends Bloc<ElderEvent, ElderState> {
+  final ElderService service;
+
+  ElderBloc(this.service) : super(ElderInitial()) {
+    on<LoadElder>((event, emit) async {
+      emit(ElderLoading());
+      try {
+        final elder = await service.fetchElder();
+        emit(ElderLoaded(elder));
+      } catch (e) {
+        emit(ElderError("Failed to load elder"));
+      }
+    });
+
+    on<SaveElder>((event, emit) async {
+      try {
+        await service.updateElder(event.elder);
+        emit(ElderSaved());
+      } catch (_) {
+        emit(ElderError("Failed to save elder"));
+      }
+    });
+  }
+}
