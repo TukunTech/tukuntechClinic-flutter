89656e5ef5990f9620458916cf1f1600e87f16ff
Erick <erickmaraza20@gmail.com>
2025-06-19 15:30:50 -0500

    feat(profile): define domain entity for Elder

---
+++ lib/features/profile/domain/entities/elder.dart
@@ -0,0 +1,27 @@
+class Elder {
+  final int id;
+  String name;
+  String lastname;
+  String dni;
+  String gender;
+  int age;
+  String nacionalidad;
+  String numeroPoliza;
+  String seguro;
+  String alergias;
+  String grupoSanguineo;
+
+  Elder({
+    required this.id,
+    required this.name,
+    required this.lastname,
+    required this.dni,
+    required this.gender,
+    required this.age,
+    required this.nacionalidad,
+    required this.numeroPoliza,
+    required this.seguro,
+    required this.alergias,
+    required this.grupoSanguineo,
+  });
+}
